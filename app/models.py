d617dc027e7432baf1c84f1a98fcaf3cb37f0ac7
Alexander-M-Waldman <awaldman1@gmail.com>
2023-04-24 12:41:51 -0400

    Updates

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import datetime
 from app import db
+
 
 # db = SQLAlchemy()
 
@@ -7,13 +10,16 @@ class Smell(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
-    smell_type = db.Column(db.String(100), nullable=False)
     intensity = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     submitter_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     smell_type_id = db.Column(db.Integer, db.ForeignKey('smell_type.id'))
     smell_source_id = db.Column(db.Integer, db.ForeignKey('smell_source.id'))
+
+    # Add a relationship property to link the Smell and SmellType models
+    smell_type = relationship("SmellType", backref="smells")
+
 
 
     def __repr__(self):
@@ -24,7 +30,11 @@ class SmellType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     description = db.Column(db.String(200))
-    smells = db.relationship('Smell', backref='smell_type_obj', lazy='dynamic')
+    # smells = db.relationship('Smell', backref='smell_type_obj', lazy='dynamic')
+    color = db.Column(db.String(7))
+
+    # Add a relationship property to link the SmellType and Smell models
+    # smells = relationship("Smell", back_populates="smell_type")
 
     def __repr__(self):
         return f'<SmellType {self.name}>'
